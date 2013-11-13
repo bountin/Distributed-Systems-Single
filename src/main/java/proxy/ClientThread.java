b7be01f301bd1f2f99f7c1b83c54ee7be1448dbe
Martin Prebio <mp@25th-floor.com>
2013-11-13 17:42:23 +0100

    Moving Proxy's TCPThread to own ClientThread class

---
+++ src/main/java/proxy/ClientThread.java
@@ -0,0 +1,151 @@
+package proxy;
+
+import message.Request;
+import message.Response;
+import message.request.*;
+import message.response.BuyResponse;
+import message.response.CreditsResponse;
+import message.response.LoginResponse;
+import message.response.MessageResponse;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+
+class ClientThread implements Runnable, IProxy {
+	private ServerSocket tcpSocket;
+	private final HashMap<String, Integer> creditList;
+	private final HashMap<String, String> passwordList;
+	String user = null;
+
+	private ClientThread(ServerSocket tcpSocket, HashMap<String, Integer> creditList, HashMap<String, String> passwordList) {
+		this.tcpSocket = tcpSocket;
+		this.creditList = creditList;
+		this.passwordList = passwordList;
+	}
+
+	static public void initNewThread(ServerSocket tcpSocket, HashMap<String, Integer> creditList, HashMap<String, String> passwordList) {
+		(new Thread(new ClientThread(tcpSocket, creditList, passwordList))).start();
+	}
+
+	public void run() {
+		Socket socket;
+		try {
+			socket = tcpSocket.accept();
+			initNewThread(tcpSocket, creditList, passwordList);
+
+			ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
+			ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+
+			Object request;
+
+			while (true) {
+				request = in.readObject();
+
+				Response response;
+
+				try {
+					if (request instanceof LoginRequest) {
+						response = login((LoginRequest) request);
+					} else if (request instanceof CreditsRequest) {
+						response = credits();
+					} else if (request instanceof BuyRequest) {
+						response = buy((BuyRequest) request);
+					} else if (request instanceof Request) {
+						response = new MessageResponse("Unsupported Request: " + request.getClass());
+					} else {
+						response = new MessageResponse("Got a non-request object");
+					}
+				} catch (NotLoggedInException unused) {
+					response = new MessageResponse("Please login first");
+				}
+
+				out.writeObject(response);
+				out.flush();
+			}
+		} catch (EOFException unused) {
+			// Client aborted connection
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public LoginResponse login(LoginRequest request) throws IOException {
+		// Check if user is known - The type enum does not support this so just write WRONG_CREDENTIALS
+		if (! passwordList.containsKey(request.getUsername())) {
+			return new LoginResponse(LoginResponse.Type.WRONG_CREDENTIALS);
+		}
+
+		// Check if passwort is correct
+		if (! passwordList.get(request.getUsername()).equals(request.getPassword())) {
+			return new LoginResponse(LoginResponse.Type.WRONG_CREDENTIALS);
+		}
+
+		user = request.getUsername();
+		return new LoginResponse(LoginResponse.Type.SUCCESS);
+	}
+
+	@Override
+	public Response credits() throws IOException {
+		checkLoginStatus();
+
+		int credits;
+		synchronized (creditList) {
+			credits = creditList.get(user);
+		}
+		return new CreditsResponse(credits);
+	}
+
+	@Override
+	public Response buy(BuyRequest credits) throws IOException {
+		checkLoginStatus();
+
+		int userCredits;
+		long creditsToBeBought = Math.max(0, credits.getCredits());
+		synchronized (creditList) {
+			userCredits = creditList.get(user);
+			userCredits += creditsToBeBought;
+			creditList.put(user, userCredits);
+		}
+		return new BuyResponse(userCredits);
+	}
+
+	@Override
+	public Response list() throws IOException {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public Response download(DownloadTicketRequest request) throws IOException {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public MessageResponse upload(UploadRequest request) throws IOException {
+		return null;  //To change body of implemented methods use File | Settings | File Templates.
+	}
+
+	@Override
+	public MessageResponse logout() throws IOException {
+		MessageResponse response = new MessageResponse("Logged out " + user);
+		user = null;
+		return response;
+	}
+
+	private void checkLoginStatus() throws NotLoggedInException {
+		if (user == null) {
+			throw new NotLoggedInException();
+		}
+	}
+
+	private class NotLoggedInException extends IOException {
+	}
+}
--- src/main/java/proxy/ProxyImpl.java
+++ src/main/java/proxy/ProxyImpl.java
@@ -1,26 +1,18 @@
 package proxy;
 
-import message.Request;
-import message.Response;
-import message.request.*;
-import message.response.BuyResponse;
-import message.response.CreditsResponse;
-import message.response.LoginResponse;
-import message.response.MessageResponse;
 import util.Config;
 
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
 
 public class ProxyImpl {
 
 	static final HashMap<String, Integer> creditList   = new HashMap<String, Integer>();
-	static HashMap<String, String>  passwordList = new HashMap<String, String>();
+	static final HashMap<String, String>  passwordList = new HashMap<String, String>();
 
 	public static void main(String[] args) throws IOException {
 		Config config = new Config("proxy");
@@ -55,137 +47,7 @@ public class ProxyImpl {
 		}
 
 		ServerSocket tcpSocket = new ServerSocket(portTCP);
-		TCPThread.initNewThread(tcpSocket);
-	}
-
-	private static class TCPThread implements Runnable, IProxy {
-		private ServerSocket tcpSocket;
-		String user = null;
-
-		private TCPThread(ServerSocket tcpSocket) {
-			this.tcpSocket = tcpSocket;
-		}
-
-		static public void initNewThread(ServerSocket tcpSocket) {
-			(new Thread(new TCPThread(tcpSocket))).start();
-		}
-
-		public void run() {
-			Socket socket;
-			try {
-				socket = tcpSocket.accept();
-				initNewThread(tcpSocket);
-
-				ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-				out.flush();
-				ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-
-				Object request;
-
-				while (true) {
-					request = in.readObject();
-
-					Response response;
-
-					try {
-						if (request instanceof LoginRequest) {
-							response = login((LoginRequest) request);
-						} else if (request instanceof CreditsRequest) {
-							response = credits();
-						} else if (request instanceof BuyRequest) {
-							response = buy((BuyRequest) request);
-						} else if (request instanceof Request) {
-							response = new MessageResponse("Unsupported Request: " + request.getClass());
-						} else {
-							response = new MessageResponse("Got a non-request object");
-						}
-					} catch (NotLoggedInException unused) {
-						response = new MessageResponse("Please login first");
-					}
-
-					out.writeObject(response);
-					out.flush();
-				}
-			} catch (EOFException unused) {
-				// Client aborted connection
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@Override
-		public LoginResponse login(LoginRequest request) throws IOException {
-			// Check if user is known - The type enum does not support this so just write WRONG_CREDENTIALS
-			if (! passwordList.containsKey(request.getUsername())) {
-				return new LoginResponse(LoginResponse.Type.WRONG_CREDENTIALS);
-			}
-
-			// Check if passwort is correct
-			if (! passwordList.get(request.getUsername()).equals(request.getPassword())) {
-				return new LoginResponse(LoginResponse.Type.WRONG_CREDENTIALS);
-			}
-
-			user = request.getUsername();
-			return new LoginResponse(LoginResponse.Type.SUCCESS);
-		}
-
-		@Override
-		public Response credits() throws IOException {
-			checkLoginStatus();
-
-			int credits;
-			synchronized (creditList) {
-				credits = creditList.get(user);
-			}
-			return new CreditsResponse(credits);
-		}
-
-		@Override
-		public Response buy(BuyRequest credits) throws IOException {
-			checkLoginStatus();
-
-			int userCredits;
-			long creditsToBeBought = Math.max(0, credits.getCredits());
-			synchronized (creditList) {
-				userCredits = creditList.get(user);
-				userCredits += creditsToBeBought;
-				creditList.put(user, userCredits);
-			}
-			return new BuyResponse(userCredits);
-		}
-
-		@Override
-		public Response list() throws IOException {
-			return null;  //To change body of implemented methods use File | Settings | File Templates.
-		}
-
-		@Override
-		public Response download(DownloadTicketRequest request) throws IOException {
-			return null;  //To change body of implemented methods use File | Settings | File Templates.
-		}
-
-		@Override
-		public MessageResponse upload(UploadRequest request) throws IOException {
-			return null;  //To change body of implemented methods use File | Settings | File Templates.
-		}
-
-		@Override
-		public MessageResponse logout() throws IOException {
-			MessageResponse response = new MessageResponse("Logged out " + user);
-			user = null;
-			return response;
-		}
-
-		private void checkLoginStatus() throws NotLoggedInException {
-			if (user == null) {
-				throw new NotLoggedInException();
-			}
-		}
-
-		private class NotLoggedInException extends IOException {
-		}
+		ClientThread.initNewThread(tcpSocket, creditList, passwordList);
 	}
 
 //	private static class UDPThread implements Runnable {
